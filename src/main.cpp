1fa9256b1fa13dd42db74fd02599caa74d9ce2bb
Salih <mustafahakan9215@gmail.com>
2025-03-07 11:25:36 +0300

    basic structure and namespace modification

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include "enet.h"
 namespace rl{
-    #include <raylib.h>
+	#include <raylib.h>
 }
+using namespace rl;
 
-int main() 
-{   
-	rl::InitWindow(rl::GetMonitorWidth(0), rl::GetMonitorHeight(0), "this took over 5 hours 😭");
-	rl::SetWindowState(rl::FLAG_WINDOW_RESIZABLE);
-	rl::SetExitKey(rl::KEY_NULL);
-	rl::MaximizeWindow();
-	rl::SetTargetFPS(144);
-    
-    while (!rl::WindowShouldClose())
-    {   
-        rl::BeginDrawing();
-            rl::ClearBackground(rl::BLACK);
-        rl::EndDrawing();
-    }
-    
-    rl::CloseWindow();
+int main(){   
+	rl::InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "this took over 5 hours 😭");
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+	SetExitKey(KEY_NULL);
+	MaximizeWindow();
+	SetTargetFPS(144);
+	
+	Vector2 pos;
+	while(!WindowShouldClose()){
+		if(IsKeyDown(KEY_W)) pos.y-= 0.5;
+		if(IsKeyDown(KEY_A)) pos.x-= 0.5;
+		if(IsKeyDown(KEY_S)) pos.y+= 0.5;
+		if(IsKeyDown(KEY_D)) pos.x+= 0.5;
+
+		BeginDrawing();
+			ClearBackground(rl::BLACK);
+			DrawRectangle(pos.x, pos.y, 100.0f, 100.0f, rl::YELLOW );
+		EndDrawing();
+	}
+	rl::CloseWindow();
 }
